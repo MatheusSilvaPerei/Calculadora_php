bbcec21c1f3d389dfbb50772b58a0c34001053f4
MatheusSilvaPerei <matheus.pereira404@etec.sp.gov.br>
2024-03-19 12:17:00 -0300

    Calculadora

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <form name="cadastro" method="POST" action="resposta.php">
+        Nome: <input type="text" name="nome"><br>
+        Endreço: <input type="text" name="endereco"><br>
+        Bairro: <input type="text" name="bairro"><br>
+        CEP: <input type="text" name="cep"><br>
+        <input type="submit" name="cadastrar" value="Cadastrar">
+    </form>
+</body>
+</html>
